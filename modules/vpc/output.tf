7b00206c2024bb6fa6e702436b3cf8059a60c66a
tay2345-gif <TashuanLawrence7@gmail.com>
2025-05-28 11:17:43 -0400

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "db_sg_id" {
+  value = aws_security_group.db.id
+}
